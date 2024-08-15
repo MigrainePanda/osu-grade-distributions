3db09f9f80f3289db2af0837f6dde622c96bb228
MigrainPanda <nicktanaka36@gmail.com>
2024-08-15 10:48:55 -0700

    update course selector with year/term filter

--- frontend-react/src/components/selectors/CourseSelector.tsx
+++ frontend-react/src/components/selectors/CourseSelector.tsx
@@ -9,27 +9,75 @@ import { OptionsType, ValueType } from "./SelectorTypes.tsx";
 function CourseSelector() {
     const [options, setOptions] = useState<OptionsType>([]);
     const [value, setValue] = useState<ValueType>([]);
-    const { subjectName, setCourseName } = useContext(CurrInfoContext);
+    const { subjectName, setCourseName, year, term } = useContext(CurrInfoContext);
     const { allCourses } = useContext(AllInfoContext);
 
-    useEffect(() => {
-        const uniqueCourses = new Set<string>();
-        allCourses.filter((course) => {
-            const subject = course['subject'] === subjectName;
-            const unique = uniqueCourses.has(course['short']);
-            if (subject && !unique) {
-                uniqueCourses.add(course['short']);
-                return true;
-            }
-            return false;
-        });
+    function addOptions(uniqueCourses: Set<string>) {
         const filtered = Array.from(uniqueCourses);
         const sorted = filtered.sort();
         const formatted = sorted.map((course) => {
             return { label: course, value: course }
         })
-        setOptions(formatted)
-    }, [allCourses, subjectName]);
+        setOptions(formatted);
+        console.log(formatted)
+        console.log(formatted.length)
+    }
+
+    useEffect(() => {
+        // just year/term, no subject
+        if (subjectName === "") {
+            return;
+        }
+        const uniqueCourses = new Set<string>();
+
+        // subject, term
+        if (term !== "N/A" && term !== "All") {
+            console.log("subject term")
+            allCourses.filter((course) => {
+                const isSubject = course['subject'] === subjectName;
+                const isYear = course['year'] === year;
+                const isTerm = course['term'] === term;
+                const isUnique = uniqueCourses.has(course['short']);
+                if (isSubject && isYear && isTerm && !isUnique) {
+                    uniqueCourses.add(course['short']);
+                    return true;
+                }
+                return false;
+            });
+            addOptions(uniqueCourses);
+            return;
+        }
+
+        // subject, year
+        if (year !== "" && year !== "All") {
+            console.log("subject year")
+            allCourses.filter((course) => {
+                const isSubject = course['subject'] === subjectName;
+                const isYear = course['year'] === year;
+                const isUnique = uniqueCourses.has(course['short']);
+                if (isSubject && isYear && !isUnique) {
+                    uniqueCourses.add(course['short']);
+                    return true;
+                }
+                return false;
+            });
+            addOptions(uniqueCourses);
+            return;
+        }
+
+        // just subject
+        console.log("subject")
+        allCourses.filter((course) => {
+            const isSubject = course['subject'] === subjectName;
+            const isUnique = uniqueCourses.has(course['short']);
+            if (isSubject && !isUnique) {
+                uniqueCourses.add(course['short']);
+                return true;
+            }
+            return false;
+        });
+        addOptions(uniqueCourses);
+    }, [allCourses, subjectName, year, term]);
 
     const handleChange = (option) => {
         console.log("course updated ", option);
