30314bc1cfb3bb3b3eda9833b1d485795635d817
MigrainPanda <nicktanaka36@gmail.com>
2024-08-20 16:50:15 -0700

    update header/footer UI

--- frontend-react/src/components/Footer/Footer.tsx
+++ frontend-react/src/components/Footer/Footer.tsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <>
             <footer>
-                <p>&copy; 2024 Nicholas Tanaka. All rights reserved.</p>
+                <p className="copyright">&copy; 2024 Nicholas Tanaka. All rights reserved.</p>
             </footer>
         </>
     );
--- frontend-react/src/components/Header/Header.tsx
+++ frontend-react/src/components/Header/Header.tsx
@@ -21,7 +21,7 @@ function Header(  ) {
         <>
             <header className="navbar-container">
                 <div className="navbar-wrapper">
-                    <NavLink className="navbar-brand" to="/" onClick={() => handleClick(0)}>OSU Grade Distributions</NavLink>
+                    <NavLink className="navbar-brand" to="/" onClick={() => handleClick(0)}>Dam Grades</NavLink>
                     <nav className="navbar-menu">
                         <NavLink className="navbar-menu-items" to="/" onClick={() => handleClick(0)}>Home</NavLink>
                         <NavLink className="navbar-menu-items" to="/courses" onClick={() => handleClick(1)}>Courses</NavLink>
