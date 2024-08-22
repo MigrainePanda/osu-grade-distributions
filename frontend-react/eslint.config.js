8f92dad015445f47475d1cef6e44384bd023cfed
MigrainPanda <nicktanaka36@gmail.com>
2024-08-22 14:42:16 -0700

    update configs for deployment

--- frontend-react/eslint.config.js
+++ frontend-react/eslint.config.js
@@ -6,12 +6,17 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config({
-  extends: [js.configs.recommended, ...tseslint.configs.recommended],
+  settings: {react: {version: '18.3'}},
+  extends: [js.configs.recommended, ...tseslint.configs.recommendedTypeChecked],
   files: ['**/*.{ts,tsx}'],
   ignores: ['dist'],
   languageOptions: {
     ecmaVersion: 2020,
     globals: globals.browser,
+    parserOptions: {
+      project: ['./tsconfig.node.json', './tsconfig.app.json'],
+      tsconfigRootDir: import.meta.dirname,
+    },
   },
   settings: { react: { version: '18.3' } },
   plugins: {
