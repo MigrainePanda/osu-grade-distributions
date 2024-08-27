342f5a3b16363607897658298975bdb5f88854c8
MigrainPanda <nicktanaka36@gmail.com>
2024-08-26 17:15:56 -0700

    implement contact page email sending/captcha

---
+++ frontend-react/src/components/Contact.tsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import useWeb3Forms from "@web3forms/react";    
+import HCaptcha from "@hcaptcha/react-hcaptcha";
+
+function Contact() {
+
+    const {register, reset, handleSubmit, setValue } = useForm();
+    const [result, setResult] = useState<string>("");
+    const accessKey = "c300c8ae-679f-4eb4-84e8-f91e7382a246";
+
+    const { submit: onSubmit } = useWeb3Forms({
+        access_key: accessKey,
+        settings: {
+        from_name: "OSU Grade Distributions",
+        subject: "New Contact Message from OSU Grade Distributions",
+        },
+        onSuccess: (msg, data) => {
+            console.log("contact success: ", msg, data)
+            setResult(msg);
+            reset();
+        },
+        onError: (msg, data) => {
+            console.log("contact error: ", msg, data)
+            setResult(msg);
+        },
+    });
+
+    const onHCaptchaChange = (token) => {
+        setValue("h-captcha-response", token);
+    };
+
+    document.getElementById('contact-message-input')?.addEventListener('input', () => updateCounter());
+    function updateCounter() {
+        const textarea = document.getElementById('contact-message-input') as HTMLInputElement;
+        const counter = document.getElementById('char-counter') as HTMLElement;
+        const numWords = textarea.value.length.toString();
+        counter.textContent = numWords;
+    }
+
+  return (
+    <>
+    <form onSubmit={handleSubmit(onSubmit)} className="contact-form">
+        <input
+            type="checkbox"
+            id=""
+            className="hidden"
+            style={{ display: "none" }}
+            {...register("botcheck")}
+        /> 
+
+        <div className="contact-input-container">
+            <label htmlFor="contact-name-input" className="page-text contact-label"><p>Full Name</p></label>
+            <input 
+                type="text"
+                placeholder="John Stones"
+                id="contact-name-input" 
+                {...register("name", { 
+                    required: "Enter your full name", 
+                    maxLength: 30
+                })} 
+            />
+        </div>
+
+            <div className="contact-input-container">
+                <label htmlFor="contact-email-input" className="page-text contact-label"><p>Email</p></label>
+                <input 
+                    type="email"
+                    placeholder="johnstones@email.com"
+                    id="contact-name-input" 
+                    {...register("email", { 
+                        required: "Enter your email", 
+                        maxLength: 50
+                    })} 
+                />
+            </div>
+
+            <div className="contact-input-container">
+                <label htmlFor="contact-message-input" className="page-text contact-label"><p>Message</p></label>
+                <textarea 
+                    id="contact-message-input" 
+                    placeholder="Please enter your message in 500 characters or less" 
+                    {...register("message", { 
+                        required: "Enter your message", 
+                        maxLength: 50,
+                    })}
+                />
+                <div className="char-counter-container">
+                    <p id="char-counter">0</p>
+                </div>
+            </div>
+
+            <div className="captcha-container">
+                <HCaptcha
+                    sitekey="50b2fe65-b00b-4b9e-ad62-3ba471098be2"
+                    reCaptchaCompat={false}
+                    onVerify={onHCaptchaChange} 
+                />
+            </div>
+
+            <div className="contact-submit-container">
+                <button type="submit" className="contact-submit-button">Submit</button>
+            </div>
+
+        </form>
+
+        <div className="contact-result page-text center-text">{result}</div>
+
+    </>
+ );
+}
+
+export default Contact;
--- frontend-react/src/pages/contact-page.tsx
+++ frontend-react/src/pages/contact-page.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 
 import "./css/contact-page.css";
+import Contact from "../components/Contact";
 
 function ContactPage() {
 
@@ -10,24 +11,6 @@ function ContactPage() {
 
     const myEmail = <a href="mailto:tanakan@oregonstate.edu" className="email-link">tanakan@oregonstate.edu</a>;
 
-    function updateCounter() {
-        const textarea = document.getElementById('contact-message-input') as HTMLInputElement;
-        const counter = document.getElementById('char-counter') as HTMLElement;
-        const numWords = textarea.value.length.toString();
-        counter.textContent = numWords;
-    }
-
-    function formSubmit(e) {
-        e.preventDefault();
-        const form = document.getElementById('contact-form') as HTMLFormElement;
-        const arr =  form.elements as HTMLFormControlsCollection
-        for (const input of arr) {
-            const ele = input as HTMLInputElement;
-            console.log(ele.value)
-            // need to submit post to do email thing
-        }
-    }
-
     return (
         <>
             <div className="contact-content">
@@ -35,49 +18,7 @@ function ContactPage() {
                 <h1 className="page-title center-text">Get in Touch!</h1>
                 <p className="page-text contact-subtitle center-text">Email me at {myEmail} or fill out the form below.</p>
 
-                <form className="contact-form" id="contact-form" onSubmit={formSubmit}>
-
-                    <div className="contact-input-container">
-                        <label htmlFor="contact-name-input" className="page-text contact-label"><p>Full Name</p></label>
-                        <input 
-                            type="text" 
-                            name="contact-name" 
-                            id="contact-name-input" 
-                            placeholder="Ex. John Stones" 
-                            maxLength={30}
-                        />
-                    </div>
-
-                    <div className="contact-input-container">
-                        <label htmlFor="contact-email-input" className="page-text contact-label"><p>Email</p></label>
-                        <input 
-                            type="text" 
-                            name="contact-email" 
-                            id="contact-email-input"
-                            placeholder="Ex. johnstones@email.com"
-                            maxLength={50}
-                        />
-                    </div>
-
-                    <div className="contact-input-container">
-                        <label htmlFor="contact-message-input" className="page-text contact-label"><p>Message</p></label>
-                        <textarea 
-                            name="contact-message" 
-                            id="contact-message-input" 
-                            placeholder="Please enter your message in 500 characters or less"
-                            maxLength={500}
-                            onChange={updateCounter}
-                        />
-                        <div className="char-counter-container">
-                            <p id="char-counter">0</p>
-                        </div>
-                    </div>
-
-                    <div className="contact-submit-container">
-                        <button className="contact-submit-button">Submit</button>
-                    </div>
-
-                </form>
+                <Contact />
 
             </div>
         </>
