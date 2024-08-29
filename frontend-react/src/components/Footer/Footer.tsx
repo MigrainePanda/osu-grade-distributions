887bf653e69a1aa13989780f51fa39daf4e1ef51
MigrainPanda <nicktanaka36@gmail.com>
2024-08-29 00:58:23 -0700

    implement simplified footer

--- frontend-react/src/components/Footer/Footer.tsx
+++ frontend-react/src/components/Footer/Footer.tsx
@@ -1,37 +1,13 @@
-import { NavLink } from "react-router-dom";
 import "./Footer.css"
 
 function Footer() {
     return (
         <>
             <footer>
-
-                <nav className="footer-inner-container">
-
-                    <div className="footer-inner-top">
-                        <p className="navbar-brand"><img src="/favicon.png" className="footer-logo"></img>OSU Grade Distributions</p>
-                        <p className="copyright">&copy; 2024 Nicholas Tanaka</p>
-                    </div>
-
-                    <div className="footer-inner-bottom">
-
-                        <div className="footer-inner-bottom-items">
-                            <p className="footer-inner-column-title">Support</p>
-                            <div className="footer-inner-column-item">
-                                <NavLink to="/contact">Contact Me</NavLink>
-                            </div>
-                        </div>
-                        
-                        <div className="footer-inner-bottom-items">
-                            <p className="footer-inner-column-title">About</p>
-                            <div className="footer-inner-column-item">
-                                <NavLink to="/about">About This Website</NavLink>
-                            </div>
-                        </div>
-                        
-                    </div>
-                    
-                </nav>
+                <div className="footer-inner-container">
+                    <p className="navbar-brand"><img src="/favicon.png" className="footer-logo"></img>OSU Grade Distributions</p>
+                    <p className="copyright">&copy; 2024 Nicholas Tanaka</p>
+                </div>
             </footer>
         </>
     );
