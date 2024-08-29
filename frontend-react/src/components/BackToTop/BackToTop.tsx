e62fd4e20473b89a2f557eaa899d82c35f8f30c5
MigrainPanda <nicktanaka36@gmail.com>
2024-08-29 01:15:21 -0700

    implement back to top styling

--- frontend-react/src/components/BackToTop/BackToTop.tsx
+++ frontend-react/src/components/BackToTop/BackToTop.tsx
@@ -1,15 +1,31 @@
+import { useEffect, useRef } from "react";
 import "./BackToTop.css";
 
 function BackToTop() {
+
+    const containerRef = useRef<HTMLDivElement>(null);
 
     function topFunction() {
         window.scrollTo({top: 0, behavior: "smooth"});
     }
 
+    function showButton() {
+        if (window.scrollY > 0) {
+            containerRef.current?.classList.add('show-top-button');
+        } 
+        else {
+            containerRef.current?.classList.remove('show-top-button');
+        }
+    }
+
+    useEffect(() => {
+        window.onscroll = function() {showButton()};
+    })
+
     return (
         <>
-            <div className="toTop-wrapper">
-                <button className="toTop-component" onClick={topFunction} >Back to Top</button>
+            <div className="toTop-container" ref={containerRef} onClick={topFunction}>
+                <img src="/arrow-up-circle-line.svg" alt="Jump to top button" className="toTop-img" />
             </div>
         </>
     );
