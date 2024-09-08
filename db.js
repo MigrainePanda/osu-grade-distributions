a8a062d3998ae1ca204ab71530552e2790a59402
MigrainPanda <nicktanaka36@gmail.com>
2024-09-07 22:09:45 -0700

    implement stg/prd database connection

--- db.js
+++ db.js
@@ -3,17 +3,27 @@ import { configDotenv } from 'dotenv';
 import { readFileSync } from 'fs';
 configDotenv();
 
-
-
 const { Pool } = pg;
+const connectionString = process.env.DATABASE_URL;
 
-const db = new Pool({
-    user: "postgres",
-    password: "password",
-    host: "localhost",
-    port: 5432,
-    database: "osu_grade_distributions"
-});
+let db;
+
+if (process.env.NODE_ENV === "staging" || process.env.NODE_ENV === "production") {
+    console.log("\n\nStaging/Production Env Database\n\n");
+    db = new Pool({
+        connectionString
+    });   
+}
+else {
+    console.log("\nDevelopment Env Database");
+    db = new Pool({
+        user: "postgres",
+        password: "password",
+        host: "localhost",
+        port: 5432,
+        database: "osu_grade_distributions"
+    });
+}
 
 try {
     const checkConn = await db.query('SELECT NOW()');
