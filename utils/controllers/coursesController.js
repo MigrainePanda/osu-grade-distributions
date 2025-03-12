3b1087a0554839077a12f7ca18f772d4f3e456d1
nick <nicktanaka36@gmail.com>
2025-03-12 13:32:56 -0700

    update query by subject, term, and year

--- utils/controllers/coursesController.js
+++ utils/controllers/coursesController.js
@@ -16,6 +16,69 @@ const getAllCourses = async () => {
     });
 };
 
+const getCourseBySpecification = async (subject, term, year) => {
+    let values = [subject];
+    const query_top = `
+        SELECT
+            courses.course_id,
+            courses.short_name,
+            courses.long_name,
+            years_has_terms.term_number,
+            years_has_terms.calendar_year
+        FROM 
+            courses
+        JOIN 
+            years_has_terms on courses.year_term_id = years_has_terms.year_term_id
+        WHERE
+            short_name = $1
+    `;
+
+    let term_str = "";
+    if (term === "ALL") {
+        values.push("%");
+        term_str = "term_number LIKE $2";
+    } else {
+        values.push(term);
+        term_str = "term_number = $2";
+    }
+    let year_str = "";
+    if (year === "ALL") {
+        values.push(0);
+        year_str = `calendar_year > $3`;
+    } else {
+        values.push(parseInt(year));
+        year_str = `calendar_year = $3`;
+    }
+    let query_mid = ` AND ${term_str} AND ${year_str}`;
+
+    const query_bot = `
+        ORDER BY
+            courses.long_name
+    `;
+
+    const query = query_top + query_mid + query_bot;
+    console.log(values);
+
+    return new Promise((resolve, reject) => {
+        db.query(query, values, (err, res) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (!res) {
+                reject(err);
+                return;
+            }
+            if (res.rows.length != 0) {
+                console.log(
+                    `All rows successfully retrieved at ${timeHelper.getCurrentEpoch()}:`
+                );
+            }
+            resolve(res.rows);
+        });
+    });
+};
+
 const getCourseByID = async (id) => {
     const query = `SELECT * FROM courses where course_id=$1`;
     const values = [id];
@@ -92,4 +155,10 @@ const deleteCourseByID = async (id) => {
     });
 };
 
-export { getAllCourses, getCourseByID, addCourse, deleteCourseByID };
+export {
+    getAllCourses,
+    getCourseBySpecification,
+    getCourseByID,
+    addCourse,
+    deleteCourseByID,
+};
